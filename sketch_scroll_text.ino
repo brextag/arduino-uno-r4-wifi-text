f35163da5a1d3cec2bc9bb96914abda0d91edaf5
john samuel <143129088+brextag@users.noreply.github.com>
2025-11-13 20:02:27 +0100

    Change displayed text to 'hello world'

--- sketch_scroll_text.ino
+++ sketch_scroll_text.ino
@@ -9,14 +9,14 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("   HABIBI COME TO DUBAI   ");
+  
   matrix.beginDraw();
 
   matrix.stroke(0xFFFFFFFF);           // White color
   matrix.textScrollSpeed(100);        // Scroll speed in msn
   matrix.textFont(Font_4x6);          // Compact font for better fit
 
-  const char text[] = "   paulo lorenzo  ";    // Add spaces for padding
+  const char text[] = "   hello world  ";    // Add spaces for padding
   matrix.beginText(0, 1, 0xFFFFFF);   // Start at x=0, y=1
   matrix.println(text);
   matrix.endText(SCROLL_LEFT);
